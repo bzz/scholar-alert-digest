1da6873ece57ae4749bb476e214d49652195b43d
urgyen <imalyanov@gmail.com>
2020-10-21 15:06:25 -0500

    frontend: add webpack

---
+++ frontend/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  mode: "development",
+  entry: [
+    "regenerator-runtime/runtime",
+    "./src/index.js",
+  ],
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+}
