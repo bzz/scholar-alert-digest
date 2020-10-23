97146d1480a0461ea32f222cc42daafaea2f3e75
urgyen <imalyanov@gmail.com>
2020-10-23 12:44:59 -0500

    frontend: apply github markdown styles to report

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {render} from "react-dom"
 
+import "github-markdown-css/github-markdown.css"
 import "style.css"
 import {get} from "request"
 import Main from "routes/Main"
