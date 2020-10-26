9087e227318ad2c4a8023862c5a74082125be98b
urgyen <imalyanov@gmail.com>
2020-10-26 16:55:55 -0600

    frontend: update data model for Main

--- frontend/src/routes/Main.js
+++ frontend/src/routes/Main.js
@@ -10,24 +10,24 @@ const Main = ({papers}) => (
     <ul className="metadata">
       <li>
         <b>Date: </b>
-        {new Date().toUTCString()}
+        {papers.unread.stats.time}
       </li>
       <li>
         <b>Unread emails: </b>
-        ?
+        {papers.unread.stats.messages}
       </li>
       <li>
         <b>Paper titles: </b>
-        {papers.unread.length}
+        {papers.unread.papers.length}
       </li>
       <li>
         <b>Unique paper titles: </b>
-        ?
+        {papers.unread.stats.papers}
       </li>
     </ul>
     <h2>New papers</h2>
     <ul>
-      {papers.unread.map(paper => (
+      {papers.unread.papers.map(paper => (
         <Paper key={paper.Title} paper={paper} />
       ))}
     </ul>
@@ -36,8 +36,17 @@ const Main = ({papers}) => (
 
 Main.propTypes = {
   papers: PropTypes.shape({
-    read: PropTypes.arrayOf(PropTypes.object),
-    unread: PropTypes.arrayOf(PropTypes.object),
+    read: PropTypes.shape({
+      papers: PropTypes.arrayOf(PropTypes.object),
+    }),
+    unread: PropTypes.shape({
+      papers: PropTypes.arrayOf(PropTypes.object),
+      stats: PropTypes.shape({
+        messages: PropTypes.number,
+        papers: PropTypes.number,
+        time: PropTypes.string,
+      }),
+    }),
   }).isRequired,
 }
 
