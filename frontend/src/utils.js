53ef0cfab13497c95c8bbc08179e0784bf7c6722
urgyen <imalyanov@gmail.com>
2020-10-27 17:37:39 -0600

    frontend: add util components Maybe and Either

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -12,3 +12,26 @@ export const createReducer = handlers => (state, action) => {
 
   return state
 }
+
+export const fromMaybe = def => f => {
+  try {
+    const res = f()
+    return res != null ? res : def
+  } catch (e) {
+    return def
+  }
+}
+
+export const Maybe = ({cond, children}) => {
+  const a = typeof cond === "function"
+
+  if (a) {
+    return fromMaybe(false)(cond) ? fromMaybe(children)(children) : null
+  }
+
+  return cond ? fromMaybe(children)(children) : null
+}
+
+export const Either = ({cond, children}) => (
+  Maybe({cond, children: children[0]}) || fromMaybe(children[1])(children[1])
+)
