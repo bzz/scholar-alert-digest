c56a973a071270c31bc8771442398c54a41e70f8
urgyen <imalyanov@gmail.com>
2020-10-30 15:25:51 -0600

    frontend: refactor app container
    + initialize reducer in the container component and pass down state and
    actions as props for better testability

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,17 +1,12 @@
-import React, {useEffect, useReducer} from "react"
-import reducer, {defaultState, actions} from "reducer"
+import React, {useEffect} from "react"
+import PropTypes from "prop-types"
 import {init, changeLabel} from "effects"
 
 import Labels from "containers/Labels"
 import Report from "containers/Report"
 
-const App = () => {
-  const [state, dispatch] = useReducer(reducer, defaultState)
-  const {setLabels, setLabel, setPapers, toggleMode} = actions(dispatch)
-
-  useEffect(() => {
-    init({setLabels, setLabel, setPapers})
-  }, [])
+const App = ({state, setLabels, setLabel, setPapers, toggleMode}) => {
+  useEffect(() => init({setLabels, setLabel, setPapers}), [])
 
   if (state.currentLabel) {
     const {stats, papers} = state.papers.unread
@@ -35,6 +30,44 @@ const App = () => {
   }
 
   return null
+}
+
+const paperProps = PropTypes.shape({
+  paper: PropTypes.shape({
+    URL: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Author: PropTypes.string.isRequired,
+    Refs: PropTypes.arrayOf(PropTypes.string).isRequired,
+    Abstract: PropTypes.shape({
+      FirstLine: PropTypes.string.isRequired,
+      Rest: PropTypes.string.isRequired,
+    }).isRequired,
+  }),
+})
+
+App.propTypes = {
+  toggleMode: PropTypes.func.isRequired,
+  setPapers: PropTypes.func.isRequired,
+  setLabel: PropTypes.func.isRequired,
+  setLabels: PropTypes.func.isRequired,
+  state: PropTypes.shape({
+    currentLabel: PropTypes.string,
+    labels: PropTypes.arrayOf(PropTypes.string).isRequired,
+    mode: PropTypes.string.isRequired,
+    papers: PropTypes.shape({
+      read: PropTypes.shape({
+        papers: PropTypes.arrayOf(paperProps),
+      }).isRequired,
+      unread: PropTypes.shape({
+        papers: PropTypes.arrayOf(paperProps),
+        stats: PropTypes.shape({
+          messages: PropTypes.number,
+          papers: PropTypes.number,
+          time: PropTypes.string,
+        }),
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
 }
 
 export default App
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,27 @@
-import React from "react"
+import React, {useReducer} from "react"
 import {render} from "react-dom"
 
 import "github-markdown-css/github-markdown.css"
 import "style.css"
+import reducer, {defaultState, actions} from "reducer"
 import App from "containers/App"
 
+const Container = () => {
+  const [state, dispatch] = useReducer(reducer, defaultState)
+  const {setLabels, setLabel, setPapers, toggleMode} = actions(dispatch)
+
+  return (
+    <App
+      state={state}
+      setLabels={setLabels}
+      setLabel={setLabel}
+      setPapers={setPapers}
+      toggleMode={toggleMode}
+    />
+  )
+}
+
 render(
-  <App />,
+  <Container />,
   document.querySelector(".container"),
 )
