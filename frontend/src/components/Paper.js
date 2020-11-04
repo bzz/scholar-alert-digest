55ff49abf028cc3008022a5aef337a9f57e18d0c
urgyen <imalyanov@gmail.com>
2020-11-04 15:14:54 -0600

    frontend: update paper component
    + italicize author names
    + show number of refs for each paper
    + show ref author's names in refs

--- frontend/src/components/Paper.js
+++ frontend/src/components/Paper.js
@@ -3,20 +3,32 @@
 import React from "react"
 import PropTypes from "prop-types"
 import {modes} from "constants"
+import {Maybe} from "utils"
 
 import "components/components.css"
 
-const PaperTitle = ({paper}) => (
-  <>
-    <a className="paper__title" href={paper.URL}>{paper.Title}</a>
-    {`, ${paper.Author} `}
-    ({paper.Refs.map((ref, i) => (
-      <a key={`${i}-${ref}`} href={`https://mail.google.com/mail/#inbox/${ref}`}>
-        {i + 1}
-      </a>
-    ))})
-  </>
-)
+const PaperTitle = ({paper}) => {
+  const refs = paper.Refs.sort((x, y) => x.Title.length > y.Title.length ? -1 : 1)
+
+  return (
+    <>
+      <a className="paper__title" href={paper.URL}>{paper.Title}</a>
+      <span className="paper__author">{`, ${paper.Author} `}</span>
+      ({`${refs.length}: `} {refs.map((ref, i, refs) => (
+        <a
+          className="paper__ref"
+          key={`${i}-${ref}`}
+          href={`https://mail.google.com/mail/#inbox/${ref}`}
+        >
+          {ref.Title || i + 1}
+          <Maybe cond={refs.length > (i + 1)}>
+            {", "}
+          </Maybe>
+        </a>
+      ))})
+    </>
+  )
+}
 
 const PaperCompact = ({paper}) => (
   <li>
@@ -52,7 +64,9 @@ const paperTypes = {
     URL: PropTypes.string.isRequired,
     Title: PropTypes.string.isRequired,
     Author: PropTypes.string.isRequired,
-    Refs: PropTypes.arrayOf(PropTypes.string).isRequired,
+    Refs: PropTypes.arrayOf(
+      PropTypes.shape({ID: PropTypes.string, Title: PropTypes.string}),
+    ).isRequired,
     Abstract: PropTypes.shape({
       FirstLine: PropTypes.string.isRequired,
       Rest: PropTypes.string.isRequired,
